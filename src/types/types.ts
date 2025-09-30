b1d21fbd0894bc52312150cbdcfd4746d5b14949
Tommaso <t.paparesta@gmail.com>
2025-09-30 19:26:29 +0200

    [ADD] file separato types

---
+++ src/types/types.ts
@@ -0,0 +1,24 @@
+export class Message {
+    sender: string = "";
+
+    content: string = "";
+    conversation_id: string = "";
+    id: string = "";
+    created_at: string = "";
+}
+
+export type Style = {
+    theme: string;
+    fontFamily: string;
+}
+
+export type Conversation = {
+    id: string;
+    title: string;
+    created_at: string;
+    favourite: boolean;
+}
+
+export type Preferences = {
+    style: Style[];
+}
